48472ee305226cb1a00ffe8f4d33a7d1b63f489a
hjm <71825356+xxphjm@users.noreply.github.com>
2023-12-24 16:50:01 +0800

    新增分析結果敘述

--- AnalysisCharts.py
+++ AnalysisCharts.py
@@ -8,11 +8,14 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 import numpy as np
+from mongodb_function import *
 
 # Channel Access Token
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 # Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+plt.rcParams['font.sans-serif']=['Arial Unicode MS']
+my_mongo_client = MongoDBClient('LINEBOT', 'ANALYSIS_DESC')
 def KeywordChart(msg,type,userId):
     keyword={'洗面乳':['很好','很讚','品質好','很溫和','不干涉'],
         '化妝水':['修復','卸妝','品質好','舒緩','不適']}
@@ -22,12 +25,10 @@ def KeywordChart(msg,type,userId):
     values=np.random.rand(5)
     # 限制到小数点后两位
     values = np.round(values, 2)
-
     # 对数组进行降序排序
     values = np.sort(values)[::-1]
     # 创建长条图
     plt.bar(categories, values, color='forestgreen')
-
     # 添加标签和标题
     plt.xlabel('關鍵字')
     plt.ylabel('分數')
@@ -39,8 +40,17 @@ def KeywordChart(msg,type,userId):
     plt.close()
     ResponseChart(temp_file_path,userId)
     
+    keywordDesc=f"{msg}這項產品有五個關鍵字由多至少分別是{keyword[type]}"
+    data={
+        'USER_ID':userId,
+        'KEYWORD_DESC':keywordDesc,
+        'TYPE':type
 
-def ResponseChart(temp_file_path,userId):
+    }
+    my_mongo_client.col.insert_one(data)
+    
+
+def ResponseChart(msg,temp_file_path,userId):
 
     # 上傳至imgur
     client_id = os.getenv('IMGUR_CLIENT_ID')
